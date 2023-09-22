84b6d36c1c6d7f2f401337065e941b5a941bb2b3
Jrm1992 <jose_roberto_6@msn.com>
2023-09-22 13:11:33 -0300

    Initial Commit

---
+++ src/test/java/com/example/crudapi/CrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
