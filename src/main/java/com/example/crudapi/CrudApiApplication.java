5df1fb70bef0408ba604b47760578ad6f369d8cb
Jrm1992 <jose_roberto_6@msn.com>
2023-09-22 13:10:40 -0300

    Initial Commit

---
+++ src/main/java/com/example/crudapi/CrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiApplication.class, args);
+	}
+
+}
